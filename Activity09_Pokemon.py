7698ff9e0b9041f51644692ef2a8036873e829cb
mitchespina1001 <sophiaespina25@gmail.com>
2022-04-02 18:05:23 +0900

    First Commit

---
+++ Activity09_Pokemon.py
@@ -0,0 +1,14 @@
+import random 
+
+Pmtype = str(input("Pokemon Type: "))
+ChaType = int(input("Charizard Type: "))
+SAttack = int(input("Special Type: "))
+Stype = str(input("Skill Type: "))
+Pwr = int(input("Power: "))
+FerLevel = int(input("Feraligatr's Level: "))
+SDef = int(input("Special Defense: "))
+Etype = str(input("Enemy Type: "))
+Gen = int(input("Generation: "))
+Wthr = str(input("Weather: "))
+Trgt = int(input("Target: "))
+
